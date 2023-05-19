c228728ed8fd9a989661d78c65cb4e12b32f257f
Kamen Kirov <75930865+KamenKirov@users.noreply.github.com>
2023-05-19 14:27:35 +0300

    Update ex1.py

--- practice1/ex1.py
+++ practice1/ex1.py
@@ -1,13 +1,1 @@
-import requests
 
-def convrt_gbp_to_usd(amount):
-    response = requests.get('https://api.exchangerate-api.com/v4/latest/GBP')
-    exchange_rates = response.json()['rates']
-    usd_rate = exchange_rates['USD']
-    usd_amount = amount * usd_rate
-
-    return usd_amount
-
-pounds = int(input('Enter: '))
-us_dollars = convrt_gbp_to_usd(pounds)
-print(f'{pounds:.2f} GBP is equivalent to {us_dollars:.2f} USD')
